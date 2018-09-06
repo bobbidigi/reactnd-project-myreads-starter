4ec3a2786ef4b6a9fed216aef969f00cbdf8b1ff
RobertAllen <bobbidigi34@gmail.com>
2018-09-05 20:30:30 -0400

    add: components

---
+++ src/book.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+class book extends Component {
+
+}
+export default book;
---
+++ src/searchPage.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+class searchPage extends Component {
+
+}
+export default searchPage;
+
