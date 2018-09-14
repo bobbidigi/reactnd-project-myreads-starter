e297383d22560c0bb9c7343a76f5daac260ce21e
RobertAllen <bobbidigi34@gmail.com>
2018-09-14 08:10:49 -0400

    display and select shelf function

--- src/searchPage.js
+++ src/searchPage.js
@@ -1,6 +1,45 @@
 import React, { Component } from 'react';
-class searchPage extends Component {
 
+
+class SearchPage extends Component {
+
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({
+      query: query 
+    })
+  }
+
+
+
+  render(){
+    return(
+    <div className="search-books">
+          <div className="search-books-bar">
+              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+
+              <div className="search-books-input-wrapper">
+                    <input 
+                    type="text" 
+                    placeholder="Search by title or author"
+                    value={this.state.query} 
+                    onChange={(event) => this.updateQuery(event.target.value)}
+                    />
+              </div>
+          </div>
+          
+          <div className="search-books-results">
+              <ol className="books-grid"></ol>
+          </div>
+    </div>
+    );
+  }
 }
-export default searchPage;
+export default SearchPage;
+
+
+
 
