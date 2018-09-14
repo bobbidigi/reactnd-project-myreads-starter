0251a9c15578566b7cb2f9f94c0717e430aabc44
RobertAllen <bobbidigi34@gmail.com>
2018-09-14 14:27:11 -0400

    search form fixed problem for books with no image

--- src/book.js
+++ src/book.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 class Book extends Component {
      render(){
-          return(
+
+        let displayedThumbnail; 
+        if (!this.props.book.imageLinks) {
+          displayedThumbnail = ''
+        }else{
+          displayedThumbnail = this.props.book.imageLinks.thumbnail
+        }
+
+          return( 
           <div className="book">
                <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 
+                  `url("${displayedThumbnail}")` }}></div>
                     <div className="book-shelf-changer">
                          <select
                           onChange={(event) => this.props.moveShelf(
--- src/searchPage.js
+++ src/searchPage.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 
+import * as BooksAPI from './BooksAPI'
+import Book from './Book';
 
 class SearchPage extends Component {
 
   state = {
-    query: ''
+    query: '',
+    searchedBooks: []
   }
 
   updateQuery = (query) => {
     this.setState({
       query: query 
     })
+    this.updateSearchedBooks(query);
   }
 
 
+  updateSearchedBooks = (query) => {
+    if(query){
+      BooksAPI.search(query).then((searchedBooks) =>{
+      this.setState({ searchedBooks: searchedBooks})
+    })
+    }else{
+      this.setState({ searchedBooks: [] })
+    }
+  }
 
   render(){
+
     return(
     <div className="search-books">
           <div className="search-books-bar">
@@ -32,7 +46,15 @@ class SearchPage extends Component {
           </div>
           
           <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid"> 
+                {this.state.searchedBooks.map(searchedBook => (
+                  <li key={searchedBook.id}>
+                    <Book
+                      book={searchedBook} 
+                    />
+                  </li>  
+                ))}
+              </ol>
           </div>
     </div>
     );
