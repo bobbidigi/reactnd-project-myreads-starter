87a0e461967609abfd6dfb23c0043d2186b3fb84
RobertAllen <bobbidigi34@gmail.com>
2018-09-17 07:41:59 -0400

    fix: shelf change for books on search and main

--- src/book.js
+++ src/book.js
@@ -19,12 +19,12 @@ class Book extends Component {
                           onChange={(event) => this.props.moveShelf(
                             this.props.book, event.target.value
                           )}
-                          value={this.props.book.currentShelf}
+                          value={this.props.currentShelf}
                           >
                               <option value="move" disabled>Move to...</option>
+                              <option value="read">Read</option>
                               <option value="currentlyReading">Currently Reading</option>
                               <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
                               <option value="none">None</option>
                          </select>
                     </div>
